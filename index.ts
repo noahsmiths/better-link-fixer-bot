8ef6348a781537adfc56cd2377446dbc558233f2
Noah Smith <nsmith2@unc.edu>
2026-04-30 23:58:48 -0400

    Finished first version

---
+++ index.ts
@@ -0,0 +1,49 @@
+import { ChannelType, Client, Events, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+    ]
+});
+
+client.on(Events.ClientReady, () => {
+    console.log("Ready and listening...");
+});
+
+const BASE_TWITTER_URL = "https://x.com/";
+const BASE_INSTAGRAM_URL = "https://www.instagram.com/reel/";
+
+client.on(Events.MessageCreate, async (msg) => {
+    console.log("HERE");
+
+    if (!msg.content.includes(BASE_TWITTER_URL) && !msg.content.includes(BASE_INSTAGRAM_URL)) {
+        return;
+    }
+    if (!msg.deletable) {
+        return;
+    }
+    if (msg.channel.type !== ChannelType.GuildText) {
+        return;
+    }
+
+    console.log("HERE2");
+
+    const tempWebhook = await msg.channel.createWebhook({
+        name: msg.author.displayName,
+        avatar: msg.author.avatarURL()
+    });
+
+    const fixedMessage = msg.content
+        .replaceAll(BASE_TWITTER_URL, "https://fixupx.com/")
+        .replaceAll(BASE_INSTAGRAM_URL, "https://kkinstagram.com/reel/");
+    await tempWebhook.send({
+        content: fixedMessage
+    });
+    await tempWebhook.delete();
+    await msg.delete();
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
